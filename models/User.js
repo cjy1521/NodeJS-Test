dda7cec4ada1d907c9011f4641c0fd624797492f
Jin <cjy1521@naver.com>
2020-06-25 12:09:36 +0900

    토큰 생성기능

--- models/User.js
+++ models/User.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
+
 
 const userSchema = mongoose.Schema({
     name: {
-        type : String,
+        type: String,
         maxlength: 50
     },
 
     email: {
-        type : String,
+        type: String,
         trim: true,
-        unique : 1
+        unique: 1
     },
     password: {
         type: String,
@@ -33,6 +37,45 @@ const userSchema = mongoose.Schema({
 
 })
 
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+//plainPassword 1234567, 암호화된 비밀번호 $2b$10$C3/4hKk8Qb2SZcPEQVEezODkJqErPxC1pDd92EU8yeKtVHWbdfi3m
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+
+    // jsonwebtoken을 이용해서 토큰을 생성하기
+    var token = jwt.sign(user._id.toHexString(), "secretToken");
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
+
 const User = mongoose.model('User', userSchema)
 
-module.exports= {User}
+module.exports = { User }
