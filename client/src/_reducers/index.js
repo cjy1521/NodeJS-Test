7ade389174d6e1a1e263b39ca13f1fddf6f2dabe
Jin <cjy1521@naver.com>
2020-06-25 19:53:53 +0900

    redux 설정

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,11 @@
+// const { combineReducers } = require("redux")
+
+import { combineReducers } from 'redux';
+// import user from './user_reducer';
+
+
+const rootReducer = combineReducers({
+    // user,
+})
+
+export default rootReducer;
